7bdd2aba7738c4815e666dbae3c0947d4cbadd4b
DDashko <DDashko@beeline.kz>
2023-02-28 12:41:31 +0600

    COS-96. Reactive transactions for insert/upsert statements;

---
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/types/CouchbaseReactiveTransactionAction.java
@@ -0,0 +1,10 @@
+package liquibase.ext.couchbase.types;
+
+import com.couchbase.client.java.transactions.ReactiveTransactionAttemptContext;
+import org.reactivestreams.Publisher;
+
+import java.util.concurrent.Flow;
+import java.util.function.Function;
+
+public interface CouchbaseReactiveTransactionAction extends Function<ReactiveTransactionAttemptContext, Publisher<?>> {
+}
