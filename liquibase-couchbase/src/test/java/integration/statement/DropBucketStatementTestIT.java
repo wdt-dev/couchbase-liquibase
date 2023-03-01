02451f07ee9f6fdba9ddfa2f4c4b0c3718f19eee
konstantin-umanets <konstantin.umanets@weigandt-consulting.com>
2023-03-01 13:12:52 +0400

    [TECH] Fix tests

--- liquibase-couchbase/src/test/java/integration/statement/DropBucketStatementTestIT.java
+++ liquibase-couchbase/src/test/java/integration/statement/DropBucketStatementTestIT.java
@@ -2,27 +2,30 @@ package integration.statement;
 
 import common.ConstantScopeTestCase;
 import liquibase.ext.couchbase.exception.BucketNotExistException;
-import liquibase.ext.couchbase.operator.ClusterOperator;
 import liquibase.ext.couchbase.statement.DropBucketStatement;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static common.constants.TestConstants.CREATE_BUCKET_TEST_NAME;
-import static common.constants.TestConstants.TEST_COLLECTION;
-import static common.constants.TestConstants.TEST_SCOPE;
 import static common.matchers.CouchBaseClusterAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 
 class DropBucketStatementTestIT extends ConstantScopeTestCase {
 
+    @BeforeEach
+    void setUp() {
+        if (!clusterOperator.isBucketExists(CREATE_BUCKET_TEST_NAME)) {
+            clusterOperator.createBucket(CREATE_BUCKET_TEST_NAME);
+        }
+    }
+
     @Test
     void Should_drop_existing_bucket() {
         DropBucketStatement statement = new DropBucketStatement(CREATE_BUCKET_TEST_NAME);
-        statement.execute(new ClusterOperator(cluster));
+        statement.execute(clusterOperator);
 
         assertThat(cluster).hasNoBucket(CREATE_BUCKET_TEST_NAME);
-        bucketOperator.createDefaultTestCollection();
     }
 
 
@@ -32,17 +35,7 @@ class DropBucketStatementTestIT extends ConstantScopeTestCase {
         DropBucketStatement statement = new DropBucketStatement(notFoundBucketName);
 
         assertThatExceptionOfType(BucketNotExistException.class)
-                .isThrownBy(() -> statement.execute(new ClusterOperator(cluster)))
+                .isThrownBy(() -> statement.execute(clusterOperator))
                 .withMessage("Bucket [%s] not exists", notFoundBucketName);
-    }
-
-    @AfterEach
-    void tearDown() {
-        if (!bucketOperator.hasScope(TEST_SCOPE)) {
-            bucketOperator.createDefaultTestScope();
-        }
-        if (!bucketOperator.hasCollectionInScope(TEST_COLLECTION, TEST_SCOPE)) {
-            bucketOperator.createDefaultTestCollection();
-        }
     }
 }
