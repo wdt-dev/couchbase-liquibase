f1f481dfeb36ff39d294ac6bcb6a44356b614dab
Roman Oborin <roman.oborin@weigandt-consulting.com>
2023-03-07 15:25:14 +0400

    Spring boot starter mvp

---
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/Constants.java
@@ -0,0 +1,5 @@
+package liquibase.ext.couchbase;
+
+public class Constants {
+    public static final String COUCHBASE_CHANGELOG_NAMESPACE = "http://www.liquibase.org/xml/ns/dbchangelog-couchbase-ext";
+}
--- liquibase-couchbase/src/main/java/liquibase/ext/couchbase/database/CouchbaseLiquibaseDatabase.java
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/database/CouchbaseLiquibaseDatabase.java
@@ -1,15 +1,12 @@
 package liquibase.ext.couchbase.database;
 
-import java.sql.Driver;
 import java.util.Properties;
 
 import liquibase.database.AbstractJdbcDatabase;
 import liquibase.database.DatabaseConnection;
 import liquibase.exception.DatabaseException;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
-
 import static liquibase.ext.couchbase.database.Constants.COUCHBASE_PREFIX;
 import static liquibase.ext.couchbase.database.Constants.COUCHBASE_PRODUCT_NAME;
 import static liquibase.ext.couchbase.database.Constants.COUCHBASE_PRODUCT_SHORT_NAME;
@@ -19,12 +16,18 @@ import static liquibase.ext.couchbase.database.Constants.DEFAULT_PORT;
 /**
  * Represents instance of {@link com.couchbase.client.java.Cluster}.<br><br>
  */
-
-@NoArgsConstructor
 @AllArgsConstructor
 public class CouchbaseLiquibaseDatabase extends AbstractJdbcDatabase {
 
     private ConnectionData connectionData;
+
+    public CouchbaseLiquibaseDatabase() {
+        connectionData = new ConnectionData(
+                "Administrator",
+                "password",
+                "couchbase://127.0.0.1"
+        );
+    }
 
     @Override
     public int getPriority() {
@@ -38,14 +41,14 @@ public class CouchbaseLiquibaseDatabase extends AbstractJdbcDatabase {
 
     @Override
     public boolean isCorrectDatabaseImplementation(DatabaseConnection conn) throws DatabaseException {
-        return getDatabaseProductName().equals(conn.getDatabaseProductName());
+        return conn.getDatabaseProductName().contains(getDatabaseProductName());
     }
 
     @Override
     public String getDefaultDriver(String url) {
         if (url.startsWith(COUCHBASE_PREFIX) ||
                 url.startsWith(COUCHBASE_SSL_PREFIX)) {
-            return CouchbaseClientDriver.class.getName();
+            return CouchbaseLiquibaseDatabase.class.getName();
         }
         throw new IllegalArgumentException(url + " driver is not supported");
     }
@@ -53,17 +56,11 @@ public class CouchbaseLiquibaseDatabase extends AbstractJdbcDatabase {
     @Override
     @SneakyThrows
     public synchronized CouchbaseConnection getConnection() {
-        CouchbaseConnection connection = (CouchbaseConnection) super.getConnection();
-        if (connection != null) {
-            return connection;
-        }
-
         CouchbaseConnection couchbaseConnection = new CouchbaseConnection();
         Properties properties = new Properties();
         properties.put("user", connectionData.getUser());
         properties.put("password", connectionData.getPassword());
-        Driver driver = new CouchbaseClientDriver();
-        couchbaseConnection.open(connectionData.getConnectionString(), driver, properties);
+        couchbaseConnection.open(connectionData.getConnectionString(), null, properties);
         setConnection(couchbaseConnection);
         return couchbaseConnection;
     }
