ea6f2ffe80fd5c59ec7d1360550c70980bc18038
Roman Oborin <roman.oborin@weigandt-consulting.com>
2023-03-07 15:28:50 +0400

    Remove unnecessary props

--- liquibase-couchbase/src/main/java/liquibase/ext/couchbase/Constants.java
+++
@@ -1,5 +0,0 @@
-package liquibase.ext.couchbase;
-
-public class Constants {
-    public static final String COUCHBASE_CHANGELOG_NAMESPACE = "http://www.liquibase.org/xml/ns/dbchangelog-couchbase-ext";
-}
