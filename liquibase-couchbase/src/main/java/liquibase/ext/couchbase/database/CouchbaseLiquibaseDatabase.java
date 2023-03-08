49cd0f46d00a17fbc878856e2b53790d148fa146
Roman Oborin <roman.oborin@weigandt-consulting.com>
2023-03-08 12:15:15 +0400

    Fix schema

--- liquibase-couchbase/src/main/java/liquibase/ext/couchbase/database/CouchbaseLiquibaseDatabase.java
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/database/CouchbaseLiquibaseDatabase.java
@@ -1,12 +1,15 @@
 package liquibase.ext.couchbase.database;
 
-import java.util.Properties;
-
 import liquibase.database.AbstractJdbcDatabase;
 import liquibase.database.DatabaseConnection;
 import liquibase.exception.DatabaseException;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.SneakyThrows;
+
+import java.util.Properties;
+
+import static java.util.Optional.ofNullable;
 import static liquibase.ext.couchbase.database.Constants.COUCHBASE_PREFIX;
 import static liquibase.ext.couchbase.database.Constants.COUCHBASE_PRODUCT_NAME;
 import static liquibase.ext.couchbase.database.Constants.COUCHBASE_PRODUCT_SHORT_NAME;
@@ -16,10 +19,13 @@ import static liquibase.ext.couchbase.database.Constants.DEFAULT_PORT;
 /**
  * Represents instance of {@link com.couchbase.client.java.Cluster}.<br><br>
  */
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CouchbaseLiquibaseDatabase extends AbstractJdbcDatabase {
 
-    private ConnectionData connectionData;
+    private final ConnectionData connectionData;
+
+    @Setter(onMethod = @__( {@Override}))
+    private DatabaseConnection connection;
 
     public CouchbaseLiquibaseDatabase() {
         connectionData = new ConnectionData(
@@ -54,8 +60,14 @@ public class CouchbaseLiquibaseDatabase extends AbstractJdbcDatabase {
     }
 
     @Override
-    @SneakyThrows
     public synchronized CouchbaseConnection getConnection() {
+        return ofNullable(connection)
+                .map(CouchbaseConnection.class::cast)
+                .orElseGet(this::getCouchbaseConnection);
+    }
+
+    @SneakyThrows
+    private CouchbaseConnection getCouchbaseConnection() {
         CouchbaseConnection couchbaseConnection = new CouchbaseConnection();
         Properties properties = new Properties();
         properties.put("user", connectionData.getUser());
