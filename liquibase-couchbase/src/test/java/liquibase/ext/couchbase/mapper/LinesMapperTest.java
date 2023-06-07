8b4fcdef95b3aedd5162c3cb206827a231debdb5
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-06-07 12:29:38 +0800

    COS-191: code review refactoring

--- liquibase-couchbase/src/test/java/liquibase/ext/couchbase/mapper/LinesMapperTest.java
+++ liquibase-couchbase/src/test/java/liquibase/ext/couchbase/mapper/LinesMapperTest.java
@@ -1,10 +1,5 @@
 package liquibase.ext.couchbase.mapper;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Stream;
-
 import liquibase.ext.couchbase.provider.DocumentKeyProvider;
 import liquibase.ext.couchbase.provider.factory.DocumentKeyProviderFactory;
 import liquibase.ext.couchbase.types.DataType;
@@ -15,6 +10,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -46,18 +46,19 @@ class LinesMapperTest {
         when(documentKeyProviderFactory.getKeyProvider(any())).thenReturn(documentKeyProvider);
         when(documentKeyProvider.getKey(any())).thenAnswer((args) -> String.valueOf(keyHolder.getAndIncrement()));
 
-        List<Document> expected = new ArrayList<>();
-        Document doc1 = new Document(String.valueOf(keyHolder.get()), new Value("{}", DataType.JSON));
-        Document doc2 = new Document(String.valueOf(keyHolder.get() + 1), new Value("{}", DataType.JSON));
-        Document doc3 = new Document(String.valueOf(keyHolder.get() + 2), new Value("{}", DataType.JSON));
-        expected.add(doc1);
-        expected.add(doc2);
-        expected.add(doc3);
-
+        List<Document> expected = createDocuments();
         List<Document> result = linesMapper.map(file);
         assertThat(result).isEqualTo(expected);
 
         verify(documentKeyProviderFactory).getKeyProvider(any());
         verify(documentKeyProvider, times(3)).getKey(any());
+    }
+
+    private List<Document> createDocuments() {
+        List<Document> documentList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            documentList.add(new Document(String.valueOf(keyHolder.get() + i), new Value("{}", DataType.JSON)));
+        }
+        return documentList;
     }
 }
