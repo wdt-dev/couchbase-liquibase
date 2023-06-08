f94757d6c43f2e36801f0f4f309aa4c044ff7f56
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-06-08 19:37:52 +0800

    COS-191: make field public

--- liquibase-couchbase/src/main/java/liquibase/ext/couchbase/configuration/CouchbaseLiquibaseConfiguration.java
+++ liquibase-couchbase/src/main/java/liquibase/ext/couchbase/configuration/CouchbaseLiquibaseConfiguration.java
@@ -20,7 +20,7 @@ public class CouchbaseLiquibaseConfiguration implements AutoloadedConfigurations
     public static final ConfigurationDefinition<Duration> LOCK_TTL_PROLONGATION;
     public static final ConfigurationDefinition<Duration> TRANSACTION_TIMEOUT;
     public static final ConfigurationDefinition<Duration> MUTATE_IN_TIMEOUT;
-    private static final ConfigurationDefinition<Boolean> IS_REACTIVE_TRANSACTIONS;
+    public static final ConfigurationDefinition<Boolean> IS_REACTIVE_TRANSACTIONS;
     public static final ConfigurationDefinition<Integer> REACTIVE_TRANSACTION_PARALLEL_THREADS;
 
     static {
