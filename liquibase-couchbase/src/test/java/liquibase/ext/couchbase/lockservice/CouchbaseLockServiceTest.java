696bd21c2cbc0269e404c857d8145c2338674ae5
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-06-08 23:01:24 +0800

    COS-191: CouchbaseLockServiceTest

--- liquibase-couchbase/src/test/java/liquibase/ext/couchbase/lockservice/CouchbaseLockServiceTest.java
+++ liquibase-couchbase/src/test/java/liquibase/ext/couchbase/lockservice/CouchbaseLockServiceTest.java
@@ -1,6 +1,10 @@
 package liquibase.ext.couchbase.lockservice;
 
-import org.junit.jupiter.api.AfterEach;
+import com.couchbase.client.core.error.CouchbaseException;
+import com.couchbase.client.java.Collection;
+import liquibase.ext.couchbase.database.CouchbaseConnection;
+import liquibase.ext.couchbase.database.CouchbaseLiquibaseDatabase;
+import liquibase.ext.couchbase.provider.ContextServiceProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,25 +13,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import liquibase.Scope;
-import liquibase.executor.Executor;
-import liquibase.executor.ExecutorService;
-import liquibase.ext.couchbase.database.CouchbaseConnection;
-import liquibase.ext.couchbase.database.CouchbaseLiquibaseDatabase;
-import liquibase.ext.couchbase.provider.ContextServiceProvider;
-import liquibase.lockservice.LockServiceFactory;
-import lombok.SneakyThrows;
-import static liquibase.ext.couchbase.executor.CouchbaseExecutor.EXECUTOR_NAME;
+import static common.constants.TestConstants.TEST_BUCKET;
+import static liquibase.ext.couchbase.configuration.CouchbaseLiquibaseConfiguration.CHANGELOG_LOCK_COLLECTION_NAME;
 import static liquibase.plugin.Plugin.PRIORITY_SPECIALIZED;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class CouchbaseLockServiceTest {
 
     @Mock
-    private Executor executor;
+    private Collection collection;
 
     @Mock
     private CouchbaseConnection connection;
@@ -39,7 +42,6 @@ public class CouchbaseLockServiceTest {
 
     private CouchbaseLockService lockService;
 
-    @SneakyThrows
     @BeforeEach
     void setUp() {
         database = new CouchbaseLiquibaseDatabase();
@@ -47,19 +49,13 @@ public class CouchbaseLockServiceTest {
         lockService = new CouchbaseLockService();
         lockService.setDatabase(database);
         lockService.setServiceProvider(serviceProvider);
-        reset();
     }
 
-    @AfterEach
-    void tearDown() {
-        reset();
-    }
-
-    private void reset() {
+/*    private void reset() {
         LockServiceFactory.reset();
         Scope.getCurrentScope().getSingleton(ExecutorService.class).setExecutor(EXECUTOR_NAME, database, executor);
         Scope.getCurrentScope().getSingleton(ExecutorService.class).reset();
-    }
+    }*/
 
     @Test
     @DisplayName("Test priority of lock service")
@@ -73,5 +69,47 @@ public class CouchbaseLockServiceTest {
         assertTrue(lockService.supports(new CouchbaseLiquibaseDatabase()));
         assertTrue(lockService.supports(database));
     }
+
+    @Test
+    void Should_init_successful_on_first_call_only() {
+        when(serviceProvider.getServiceCollection(CHANGELOG_LOCK_COLLECTION_NAME.getCurrentValue())).thenReturn(collection);
+        when(collection.bucketName()).thenReturn(TEST_BUCKET);
+
+        assertThatCode(() -> lockService.init()).doesNotThrowAnyException();
+
+        boolean hasLock = lockService.hasChangeLogLock();
+        assertThat(hasLock).isFalse();
+
+        verify(serviceProvider).getServiceCollection(any());
+        reset(serviceProvider);
+
+        assertThatCode(() -> lockService.init()).doesNotThrowAnyException();
+
+        verify(serviceProvider, never()).getServiceCollection(any());
+    }
+
+    @Test
+    void Should_release_lock_successfully() {
+        when(serviceProvider.getServiceCollection(CHANGELOG_LOCK_COLLECTION_NAME.getCurrentValue())).thenReturn(collection);
+        when(collection.bucketName()).thenReturn(TEST_BUCKET);
+        when(collection.get(any())).thenThrow(new CouchbaseException("Mocked"));
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+
+        boolean acquiredLock = lockService.acquireLock();
+        assertThat(acquiredLock).isTrue();
+
+        assertThat(lockService.hasChangeLogLock()).isTrue();
+
+        verify(collection).insert(any(), any(), any());
+
+        boolean secondAcquiredLock = lockService.acquireLock();
+        assertThat(secondAcquiredLock).isEqualTo(acquiredLock);
+
+        assertThatCode(() -> lockService.releaseLock()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+    }
+
 
 }
