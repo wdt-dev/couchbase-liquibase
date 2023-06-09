f20b5d1468b7e638c317f89a607d3969b179e9e4
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-06-09 11:09:38 +0800

    COS-191: CouchbaseLockServiceTest

--- liquibase-couchbase/src/test/java/liquibase/ext/couchbase/lockservice/CouchbaseLockServiceTest.java
+++ liquibase-couchbase/src/test/java/liquibase/ext/couchbase/lockservice/CouchbaseLockServiceTest.java
@@ -2,9 +2,11 @@ package liquibase.ext.couchbase.lockservice;
 
 import com.couchbase.client.core.error.CouchbaseException;
 import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.kv.GetResult;
 import liquibase.ext.couchbase.database.CouchbaseConnection;
 import liquibase.ext.couchbase.database.CouchbaseLiquibaseDatabase;
 import liquibase.ext.couchbase.provider.ContextServiceProvider;
+import liquibase.lockservice.DatabaseChangeLogLock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,12 +33,14 @@ public class CouchbaseLockServiceTest {
 
     @Mock
     private Collection collection;
-
     @Mock
     private CouchbaseConnection connection;
-
     @Mock
     private ContextServiceProvider serviceProvider;
+    @Mock
+    private CouchbaseLock couchbaseLock;
+    @Mock
+    private GetResult getResult;
 
     private CouchbaseLiquibaseDatabase database;
 
@@ -51,12 +55,6 @@ public class CouchbaseLockServiceTest {
         lockService.setServiceProvider(serviceProvider);
     }
 
-/*    private void reset() {
-        LockServiceFactory.reset();
-        Scope.getCurrentScope().getSingleton(ExecutorService.class).setExecutor(EXECUTOR_NAME, database, executor);
-        Scope.getCurrentScope().getSingleton(ExecutorService.class).reset();
-    }*/
-
     @Test
     @DisplayName("Test priority of lock service")
     void testPriority() {
@@ -68,6 +66,14 @@ public class CouchbaseLockServiceTest {
     void testSupport() {
         assertTrue(lockService.supports(new CouchbaseLiquibaseDatabase()));
         assertTrue(lockService.supports(database));
+    }
+
+    @Test
+    void Should_list_empty_locks_array() {
+        DatabaseChangeLogLock[] result = lockService.listLocks();
+
+        assertThat(result).isNotNull();
+        assertThat(result).isEmpty();
     }
 
     @Test
@@ -90,26 +96,110 @@ public class CouchbaseLockServiceTest {
 
     @Test
     void Should_release_lock_successfully() {
-        when(serviceProvider.getServiceCollection(CHANGELOG_LOCK_COLLECTION_NAME.getCurrentValue())).thenReturn(collection);
-        when(collection.bucketName()).thenReturn(TEST_BUCKET);
+        // Is required to get FREE status of lock to release it easier.
         when(collection.get(any())).thenThrow(new CouchbaseException("Mocked"));
 
-        assertThat(lockService.hasChangeLogLock()).isFalse();
-
-        boolean acquiredLock = lockService.acquireLock();
-        assertThat(acquiredLock).isTrue();
-
-        assertThat(lockService.hasChangeLogLock()).isTrue();
-
-        verify(collection).insert(any(), any(), any());
+        acquireLock();
 
         boolean secondAcquiredLock = lockService.acquireLock();
-        assertThat(secondAcquiredLock).isEqualTo(acquiredLock);
+        assertThat(secondAcquiredLock).isTrue();
 
         assertThatCode(() -> lockService.releaseLock()).doesNotThrowAnyException();
 
         assertThat(lockService.hasChangeLogLock()).isFalse();
     }
 
+    @Test
+    void Should_force_release_lock_successfully() {
+        acquireLock();
+
+        assertThatCode(() -> lockService.forceReleaseLock()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+        verify(collection).remove(TEST_BUCKET);
+    }
+
+    @Test
+    void Should_release_lock_on_destroy_successfully() {
+        mockLockOwner(lockService.getServiceId());
+
+        acquireLock();
+
+        boolean secondAcquiredLock = lockService.acquireLock();
+        assertThat(secondAcquiredLock).isTrue();
+
+        assertThatCode(() -> lockService.destroy()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+
+        verify(collection).remove(TEST_BUCKET);
+        verify(couchbaseLock).getOwner();
+    }
+
+    @Test
+    void Should_try_release_lock_on_destroy_and_catch_exception() {
+        mockLockOwner("Owner");
+
+        acquireLock();
+
+        boolean secondAcquiredLock = lockService.acquireLock();
+        assertThat(secondAcquiredLock).isTrue();
+
+        assertThatCode(() -> lockService.destroy()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isTrue();
+
+        verify(couchbaseLock).getOwner();
+    }
+
+    @Test
+    void Should_release_lock_on_reset_successfully() {
+        mockLockOwner(lockService.getServiceId());
+
+        acquireLock();
+
+        boolean secondAcquiredLock = lockService.acquireLock();
+        assertThat(secondAcquiredLock).isTrue();
+
+        assertThatCode(() -> lockService.reset()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+
+        verify(collection).remove(TEST_BUCKET);
+        verify(couchbaseLock).getOwner();
+    }
+
+    @Test
+    void Should_try_release_lock_on_reset_and_catch_exception() {
+        mockLockOwner("Owner");
+
+        acquireLock();
+
+        boolean secondAcquiredLock = lockService.acquireLock();
+        assertThat(secondAcquiredLock).isTrue();
+
+        assertThatCode(() -> lockService.reset()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isTrue();
+
+        verify(couchbaseLock).getOwner();
+    }
+
+    private void acquireLock() {
+        when(serviceProvider.getServiceCollection(CHANGELOG_LOCK_COLLECTION_NAME.getCurrentValue())).thenReturn(collection);
+        when(collection.bucketName()).thenReturn(TEST_BUCKET);
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+        assertThat(lockService.acquireLock()).isTrue();
+        assertThat(lockService.hasChangeLogLock()).isTrue();
+
+        verify(collection).insert(any(), any(), any());
+    }
+
+    private void mockLockOwner(String owner) {
+        when(collection.get(TEST_BUCKET)).thenReturn(getResult);
+        when(getResult.contentAs(CouchbaseLock.class)).thenReturn(couchbaseLock);
+        when(couchbaseLock.getOwner()).thenReturn(owner);
+    }
 
 }
