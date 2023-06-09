3bb169a11b9141a864d3842f4d4c89ecb3388b06
Dmitry Shanko <Dmitry.Shanko@tesco.com>
2023-06-09 12:33:25 +0800

    COS-191: CouchbaseLockServiceTest

--- liquibase-couchbase/src/test/java/liquibase/ext/couchbase/lockservice/CouchbaseLockServiceTest.java
+++ liquibase-couchbase/src/test/java/liquibase/ext/couchbase/lockservice/CouchbaseLockServiceTest.java
@@ -1,8 +1,12 @@
 package liquibase.ext.couchbase.lockservice;
 
-import com.couchbase.client.core.error.CouchbaseException;
+import com.couchbase.client.core.error.DocumentExistsException;
+import com.couchbase.client.core.error.context.ErrorContext;
 import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.kv.GetResult;
+import com.couchbase.client.java.kv.MutationResult;
+import liquibase.exception.LockException;
+import liquibase.ext.couchbase.configuration.CouchbaseLiquibaseConfiguration;
 import liquibase.ext.couchbase.database.CouchbaseConnection;
 import liquibase.ext.couchbase.database.CouchbaseLiquibaseDatabase;
 import liquibase.ext.couchbase.provider.ContextServiceProvider;
@@ -10,26 +14,36 @@ import liquibase.lockservice.DatabaseChangeLogLock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static common.constants.TestConstants.TEST_BUCKET;
 import static liquibase.ext.couchbase.configuration.CouchbaseLiquibaseConfiguration.CHANGELOG_LOCK_COLLECTION_NAME;
 import static liquibase.plugin.Plugin.PRIORITY_SPECIALIZED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@MockitoSettings
 public class CouchbaseLockServiceTest {
+
+    private static final Long WAIT_TIME_MILLIS = 1000L;
+    private static final Long RECHECK_TIME_MILLIS = 100L;
 
     @Mock
     private Collection collection;
@@ -48,11 +62,17 @@ public class CouchbaseLockServiceTest {
 
     @BeforeEach
     void setUp() {
-        database = new CouchbaseLiquibaseDatabase();
-        database.setConnection(connection);
-        lockService = new CouchbaseLockService();
-        lockService.setDatabase(database);
-        lockService.setServiceProvider(serviceProvider);
+        try (MockedStatic<CouchbaseLiquibaseConfiguration> mockedStatic = Mockito.mockStatic(CouchbaseLiquibaseConfiguration.class)) {
+            mockedStatic.when(CouchbaseLiquibaseConfiguration::getChangelogWaitTime)
+                    .thenReturn(Duration.ofMillis(WAIT_TIME_MILLIS));
+            mockedStatic.when(CouchbaseLiquibaseConfiguration::getChangelogRecheckTime)
+                    .thenReturn(Duration.ofMillis(RECHECK_TIME_MILLIS));
+            database = new CouchbaseLiquibaseDatabase();
+            database.setConnection(connection);
+            lockService = new CouchbaseLockService();
+            lockService.setDatabase(database);
+            lockService.setServiceProvider(serviceProvider);
+        }
     }
 
     @Test
@@ -78,8 +98,7 @@ public class CouchbaseLockServiceTest {
 
     @Test
     void Should_init_successful_on_first_call_only() {
-        when(serviceProvider.getServiceCollection(CHANGELOG_LOCK_COLLECTION_NAME.getCurrentValue())).thenReturn(collection);
-        when(collection.bucketName()).thenReturn(TEST_BUCKET);
+        initMocks();
 
         assertThatCode(() -> lockService.init()).doesNotThrowAnyException();
 
@@ -95,14 +114,20 @@ public class CouchbaseLockServiceTest {
     }
 
     @Test
-    void Should_release_lock_successfully() {
-        // Is required to get FREE status of lock to release it easier.
-        when(collection.get(any())).thenThrow(new CouchbaseException("Mocked"));
-
+    void Should_acquire_lock_successfully_and_do_nothing_if_acquired() {
         acquireLock();
 
-        boolean secondAcquiredLock = lockService.acquireLock();
-        assertThat(secondAcquiredLock).isTrue();
+        reset(collection);
+
+        assertThat(lockService.acquireLock()).isTrue();
+
+        verify(collection, never()).insert(any(), any(), any());
+    }
+
+    @Test
+    void Should_release_lock_successfully() {
+        mockLockOwner(lockService.getServiceId());
+        acquireLock();
 
         assertThatCode(() -> lockService.releaseLock()).doesNotThrowAnyException();
 
@@ -125,9 +150,6 @@ public class CouchbaseLockServiceTest {
 
         acquireLock();
 
-        boolean secondAcquiredLock = lockService.acquireLock();
-        assertThat(secondAcquiredLock).isTrue();
-
         assertThatCode(() -> lockService.destroy()).doesNotThrowAnyException();
 
         assertThat(lockService.hasChangeLogLock()).isFalse();
@@ -142,9 +164,6 @@ public class CouchbaseLockServiceTest {
 
         acquireLock();
 
-        boolean secondAcquiredLock = lockService.acquireLock();
-        assertThat(secondAcquiredLock).isTrue();
-
         assertThatCode(() -> lockService.destroy()).doesNotThrowAnyException();
 
         assertThat(lockService.hasChangeLogLock()).isTrue();
@@ -157,9 +176,6 @@ public class CouchbaseLockServiceTest {
         mockLockOwner(lockService.getServiceId());
 
         acquireLock();
-
-        boolean secondAcquiredLock = lockService.acquireLock();
-        assertThat(secondAcquiredLock).isTrue();
 
         assertThatCode(() -> lockService.reset()).doesNotThrowAnyException();
 
@@ -175,9 +191,6 @@ public class CouchbaseLockServiceTest {
 
         acquireLock();
 
-        boolean secondAcquiredLock = lockService.acquireLock();
-        assertThat(secondAcquiredLock).isTrue();
-
         assertThatCode(() -> lockService.reset()).doesNotThrowAnyException();
 
         assertThat(lockService.hasChangeLogLock()).isTrue();
@@ -185,9 +198,39 @@ public class CouchbaseLockServiceTest {
         verify(couchbaseLock).getOwner();
     }
 
-    private void acquireLock() {
+    @Test
+    void Should_wait_for_lock_successfully() {
+        initMocks();
+        prepareMockedCollectionTimeout(3);
+
+        assertThatCode(() -> lockService.waitForLock()).doesNotThrowAnyException();
+
+        assertThat(lockService.hasChangeLogLock()).isTrue();
+
+        verify(collection, times(3)).insert(eq(TEST_BUCKET), any(), any());
+    }
+
+    @Test
+    void Should_fail_while_wait_for_lock_due_to_time_limit() {
+        initMocks();
+        prepareMockedCollectionTimeout(12);
+
+        assertThatExceptionOfType(LockException.class)
+                .isThrownBy(() -> lockService.waitForLock())
+                .withMessage("Could not acquire lock");
+
+        assertThat(lockService.hasChangeLogLock()).isFalse();
+
+        verify(collection, atLeast(10)).insert(eq(TEST_BUCKET), any(), any());
+    }
+
+    private void initMocks() {
         when(serviceProvider.getServiceCollection(CHANGELOG_LOCK_COLLECTION_NAME.getCurrentValue())).thenReturn(collection);
         when(collection.bucketName()).thenReturn(TEST_BUCKET);
+    }
+
+    private void acquireLock() {
+        initMocks();
 
         assertThat(lockService.hasChangeLogLock()).isFalse();
         assertThat(lockService.acquireLock()).isTrue();
@@ -202,4 +245,16 @@ public class CouchbaseLockServiceTest {
         when(couchbaseLock.getOwner()).thenReturn(owner);
     }
 
+    private void prepareMockedCollectionTimeout(int times) {
+        AtomicInteger incr = new AtomicInteger();
+        when(collection.insert(any(), any(), any())).thenAnswer((arg) -> {
+            if (incr.incrementAndGet() >= times) {
+                return mock(MutationResult.class);
+            }
+            else {
+                throw new DocumentExistsException(mock(ErrorContext.class));
+            }
+        });
+
+    }
 }
